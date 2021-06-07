41f8d49657cadb7cd020238cb0018e5c9ba3794b
Your Name <bonacchigerman@gmail.com>
2021-06-07 19:59:27 -0300

    problems with events

--- react/QuantityOnCart.tsx
+++ react/QuantityOnCart.tsx
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import { useProduct } from 'vtex.product-context'
 
-import { useQuery } from 'react-apollo'
+import {useQuery} from 'react-apollo'
+//import {useQuery, useLazyQuery } from 'react-apollo'
 
 import getOrderForm from './graphql/getOrderForm.gql'
+
+import { canUseDOM } from 'vtex.render-runtime'
+
+import {PixelMessage,} from './typings/events'
+
 
 interface QuantityOnCartProps {
 }
@@ -14,13 +20,27 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
   const [itemQuantity, setItemQuantity]: any = useState(null)
 
   const selectedQuantity = productContextValue?.selectedQuantity
-  const buyButton = productContextValue?.buyButton
+  //const buyButton = productContextValue?.buyButton
 
-  const {
-    data: dataGetOrderForm,
-    loading: loadingGetOrderForm,
-    error: errorGetOrderForm,
-  } = useQuery(getOrderForm)
+  const [itemsCartUpdate, setItemsCartUpdate]: any = useState(null)
+
+  const { loading: loadingGetOrderForm, error: errorGetOrderForm, data: dataGetOrderForm } = useQuery(getOrderForm)
+  
+  function handleEvents(e: PixelMessage) {
+    switch (e.data.eventName) {
+      case 'vtex:addToCart': {
+        setItemsCartUpdate(e.data)        
+        return
+      }
+      default: {
+        break
+      }
+    }
+  }
+  
+  if (canUseDOM) {
+    window.addEventListener('message', handleEvents)
+  }
 
   useEffect ( () => {
     if(loadingGetOrderForm){
@@ -38,23 +58,28 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
   )
 
   useEffect ( () => {
+    if(itemsCartUpdate){
+      const itemsOrderForm = itemsCartUpdate.items
+      const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
+      setItemQuantity(itemFound?.quantity)
+      //setItemsCartUpdate(null)        
+    }
+  },[itemsCartUpdate]
+  )
+
+  /*useEffect ( () => {
     if(buyButton.clicked){
       setItemQuantity(itemQuantity+selectedQuantity)
     }
   },[buyButton]
   )
-
-
+  */
   return(
     <div>
-        {itemQuantity && `Ya tiene ${itemQuantity} en el carrito`}
-
-        
+        {itemQuantity && `Ya tienes ${itemQuantity} en el carrito`}  
     </div>
   )
 }
-
-
 
 QuantityOnCart.schema = {
   title: 'editor.quantity-on-cart.title',
@@ -64,3 +89,4 @@ QuantityOnCart.schema = {
 }
 
 export default QuantityOnCart
+
