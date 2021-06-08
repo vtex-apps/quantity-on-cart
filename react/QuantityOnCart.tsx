9b0295fdd3de02c1b5d20aed87a23b36a186c172
Favric <favrimz@gmail.com>
2021-06-08 10:14:39 -0300

    Try to identify bug

--- react/QuantityOnCart.tsx
+++ react/QuantityOnCart.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import { useProduct } from 'vtex.product-context'
+import {useProduct} from 'vtex.product-context'
 
 import {useQuery} from 'react-apollo'
 //import {useQuery, useLazyQuery } from 'react-apollo'
 
 import getOrderForm from './graphql/getOrderForm.gql'
 
-import { canUseDOM } from 'vtex.render-runtime'
+import {canUseDOM} from 'vtex.render-runtime'
 
 import {PixelMessage,} from './typings/events'
 
@@ -15,125 +15,123 @@ interface QuantityOnCartProps {
 }
 
 const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) => {
-  const productContextValue = useProduct()
-  const productId = productContextValue?.product?.items[0]?.itemId
-  const [itemQuantity, setItemQuantity]: any = useState(null)
-  const [buyButton, setBuyButton]: any = useState(null)
-  const [itemsCartUpdate, setItemsCartUpdate]: any = useState(null)
+    const productContextValue = useProduct()
+    const productId = productContextValue?.product?.items[0]?.itemId
+    const [itemQuantity, setItemQuantity]: any = useState(null)
+    const [buyButton, setBuyButton]: any = useState(null)
+    const [itemsCartUpdate, setItemsCartUpdate]: any = useState(null)
 
-  const [itemsCartRemove, setItemsCartRemove]: any = useState(null)
+    const [itemsCartRemove, setItemsCartRemove]: any = useState(null)
 
-  const { data: dataGetOrderForm } = useQuery(getOrderForm)
+    const {data: dataGetOrderForm} = useQuery(getOrderForm)
 
-  function handleEvents(e: PixelMessage) {
-    switch (e.data.eventName) {
-      case 'vtex:addToCart': {
-        if(e.data.id && e.data.id == "add-to-cart-button"){
-          setBuyButton(e.data)
-        }else{
-          setItemsCartUpdate(e.data)        
+    function handleEvents(e: PixelMessage) {
+        switch (e.data.eventName) {
+            case 'vtex:addToCart': {
+                if (e.data.id && e.data.id === "add-to-cart-button") {
+                    setBuyButton(e.data)
+                } else {
+                    setItemsCartUpdate(e.data)
+                }
+                return
+            }
+            case 'vtex:removeFromCart': {
+                setItemsCartRemove(e.data)
+                return
+            }
+            default: {
+                break
+            }
         }
-        return
-      }
-      case 'vtex:removeFromCart': {
-        setItemsCartRemove(e.data)
-        return
-      }
-      default: {
-        break
-      }
     }
-  }
-  
-  if (canUseDOM) {
-    window.addEventListener('message', handleEvents)
-  }
 
-  useEffect ( () => {
-    if(dataGetOrderForm){
-      const itemsOrderForm = dataGetOrderForm.orderForm.items
-      const itemFound = itemsOrderForm?.find((element: { id: any }) => element.id == productId);
-      setItemQuantity(itemFound?.quantity)
+    if (canUseDOM) {
+        window.addEventListener('message', handleEvents)
     }
-  },[dataGetOrderForm]
-  )
 
-  useEffect ( () => {
-    if(itemsCartUpdate){
-      const itemsOrderForm = itemsCartUpdate.items
-      const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
-      if(itemFound?.quantity){
-        setItemQuantity(itemFound?.quantity)
-      }else{
-        setItemQuantity(itemQuantity)
-      }
-    }else{
-      setItemQuantity(itemQuantity)
-    }
-  },[itemsCartUpdate]
-  )
+    useEffect(() => {
+            console.log('me llamo on state change');
+            if (dataGetOrderForm) {
+                const itemsOrderForm = dataGetOrderForm.orderForm.items
+                const itemFound = itemsOrderForm?.find((element: { id: any }) => element.id == productId);
+                setItemQuantity(itemFound?.quantity)
+            }
+        }, [dataGetOrderForm]
+    )
 
-  useEffect ( () => {
-    if(buyButton && itemQuantity){
-      const itemsOrderForm = buyButton.items
-      const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
-      if(itemFound?.quantity){
-        setItemQuantity(itemQuantity+itemFound?.quantity)
-      }else{
-        setItemQuantity(itemQuantity)
-      }
-    }else if(buyButton && !itemQuantity) {
-      const itemsOrderForm = buyButton.items
-      const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
-      if(itemFound?.quantity){
-        setItemQuantity(0+itemFound?.quantity)
-      }
-    }else {
-      setItemQuantity(itemQuantity)
-    }
-      /*
-      }else if (!buyButton && itemQuantity){
-        console.log("aca",productId)
-        setItemQuantity(itemQuantity)
-      }else{
-        setItemQuantity(0)
-        console.log("aca2",productId)
-      }
-      */
+    useEffect(() => {
+            if (itemsCartUpdate) {
+                const itemsOrderForm = itemsCartUpdate.items
+                const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
+                if (itemFound?.quantity) {
+                    setItemQuantity(itemFound?.quantity)
+                } else {
+                    setItemQuantity(itemQuantity)
+                }
+            } else {
+                setItemQuantity(itemQuantity)
+            }
+        }, [itemsCartUpdate]
+    )
 
-    setBuyButton(false)
-  },[buyButton]
-  )
+    useEffect(() => {
+            if (buyButton && itemQuantity) {
+                const itemsOrderForm = buyButton.items
+                const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
+                if (itemFound?.quantity) {
+                    setItemQuantity(itemQuantity + itemFound?.quantity)
+                } else {
+                    setItemQuantity(itemQuantity)
+                }
+            } else if (buyButton && !itemQuantity) {
+                const itemsOrderForm = buyButton.items
+                const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
+                if (itemFound?.quantity) {
+                    setItemQuantity(0 + itemFound?.quantity)
+                }
+            } else {
+                setItemQuantity(itemQuantity)
+            }
+            /*
+            }else if (!buyButton && itemQuantity){
+              console.log("aca",productId)
+              setItemQuantity(itemQuantity)
+            }else{
+              setItemQuantity(0)
+              console.log("aca2",productId)
+            }
+            */
 
-  useEffect ( () => {
-    if(itemsCartRemove){
-      const itemsOrderForm = itemsCartRemove.items
-      const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
-      if(itemFound?.quantity){
-        setItemQuantity(0)
-      }
-    }
-  },[itemsCartRemove]
-  )
+            setBuyButton(false)
+        }, [buyButton]
+    )
 
-  var mensaje
-  if(itemQuantity && itemQuantity > 0){
-    mensaje = `Ya tienes ${itemQuantity} en el carrito`
-  }else{
-    mensaje = ``
-  }
-  return(
-    <div>
-        {(!(itemQuantity === "undefined") && itemQuantity > 0) && mensaje}  
-    </div>
-  )
+    useEffect(() => {
+            if (itemsCartRemove) {
+                const itemsOrderForm = itemsCartRemove.items
+                const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId == productId);
+                if (itemFound?.quantity) {
+                    setItemQuantity(0)
+                }
+            }
+        }, [itemsCartRemove]
+    )
+
+    let mensaje = (itemQuantity && itemQuantity > 0) ? `Tienes ${itemQuantity} en el carrito` : ''
+    return (
+        <div>
+            <p>
+                { mensaje }
+            </p>
+        </div>
+    )
 }
 
 QuantityOnCart.schema = {
-  title: 'editor.quantity-on-cart.title',
-  description: 'editor.quantity-on-cart.description',
-  type: 'object',
-  properties: {},
+    title: 'editor.quantity-on-cart.title',
+    description: 'editor.quantity-on-cart.description',
+    type: 'object',
+    properties: {},
 }
 
 export default QuantityOnCart
