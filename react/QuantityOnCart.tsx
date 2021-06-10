9fc5d6981b82f7db955f62a2a60635442f2f1265
Your Name <bonacchigerman@gmail.com>
2021-06-10 17:00:03 -0300

    fix the server render problem

--- react/QuantityOnCart.tsx
+++ react/QuantityOnCart.tsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react'
 import {useProduct} from 'vtex.product-context'
 
 import {useQuery} from 'react-apollo'
-//import {useQuery, useLazyQuery } from 'react-apollo'
 
 import getOrderForm from './graphql/getOrderForm.gql'
 
@@ -16,20 +15,21 @@ interface QuantityOnCartProps {
 
 const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) => {
     const productContextValue = useProduct()
+    
     const productId = productContextValue?.product?.items[0]?.itemId
     
     const [itemQuantity, setItemQuantity]: any = useState(null)
-
+    
     const [itemsCartRemove, setItemsCartRemove]: any = useState(null)
 
     const [itemsCartChange, setItemsCartChange]: any = useState(null)
+    
+    const [itemsCartChangeBuyBotton, setItemsCartChangeBuyBotton]: any = useState(null)
 
     const [mensaje, setMensaje]: any = useState(itemQuantity)
-
-    const {data: dataGetOrderForm, refetch, called} = useQuery(getOrderForm)
-    //const {data: dataGetOrderForm, refetch, called} = useQuery(getOrderForm)
-
-    //getOrderFormQuery()
+    
+    const {data: dataGetOrderForm, refetch, called} = useQuery(getOrderForm)    
+    
     useEffect(() => {
         if (dataGetOrderForm && productContextValue) {
             const itemsOrderForm = dataGetOrderForm.orderForm.items
@@ -38,6 +38,7 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
         }
     }, [dataGetOrderForm]
     )
+    
 
     useEffect(() => {
         if (productContextValue){
@@ -56,33 +57,24 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
             }
             case 'vtex:pageView': {             
                 if (dataGetOrderForm && productId && called){
-                    //console.log("pageView",e)
-                    //problemas en este caso
                     refetch()
-                    //setItemsCartChange(e.data)
                 }
                 return
             }
-            
             case 'vtex:cartChanged': {
-                if (dataGetOrderForm && productContextValue/* && called*/){
-                    //console.log("cartChanged")
+                if (dataGetOrderForm && productContextValue){
                     setItemsCartChange(e.data)
                 }
                 return
             }
-            
-            case 'vtex:productView': {             
-                //if (dataGetOrderForm && productId/* && called*/){
-                    //console.log("productView",e)
-                    //problemas en este caso
-                    //setItemsCartChange(e.data)
-                //}
+            case 'vtex:addToCart': {
+                if (e.data.id && e.data.id === "add-to-cart-button"){
+                    if (dataGetOrderForm && productContextValue){
+                        setItemsCartChangeBuyBotton(e.data)
+                    }
+                }
                 return
             }
-
-
-
             default: {
                 break
             }
@@ -98,6 +90,19 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
                 }
             }
         }, [itemsCartChange]
+    )
+
+    useEffect(() => {
+        if (itemsCartChangeBuyBotton) {
+            const itemsOrderForm = itemsCartChangeBuyBotton.items
+            const itemFound = itemsOrderForm?.find((element: { productId: any }) => element.productId === productId);
+            if (itemFound?.quantity) {
+                setItemQuantity(itemQuantity + itemFound?.quantity)
+            } else {
+                setItemQuantity(itemQuantity)
+            }
+        }
+    }, [itemsCartChangeBuyBotton]
     )
 
     useEffect(() => {
@@ -117,7 +122,7 @@ const QuantityOnCart: StorefrontFunctionComponent<QuantityOnCartProps> = ({}) =>
             }
     }, [itemQuantity]
     )
-
+    
     return (
         <div>
             <p>
